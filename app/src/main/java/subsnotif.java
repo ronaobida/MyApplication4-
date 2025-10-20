e335504877d4bc4222674ee441c7e78abb061d49
Ann Dominique <estrada.ad@outlook.com>
2025-10-20 15:43:08 +0800

    eto na

--- app/src/main/java/subsnotif.java
+++ app/src/main/java/subsnotif.java
@@ -4,17 +4,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapplication.R;
 
-
-public class subsnav extends Fragment {
+public class subsnotif extends Fragment{
     @NonNull
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.subscnav_fragment, container, false);
+        View view = inflater.inflate(R.layout.subsnotif_fragment, container, false);
         return view;
     }
 }
