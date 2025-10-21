1400cf0eb966315d0ce6f25353d996f2f836b1c6
Chay <gyrucheia@gmail.com>
2025-10-21 23:36:36 +0800

    eto na

--- app/src/main/java/subspayment.java
+++ app/src/main/java/subspayment.java
@@ -1,26 +1,17 @@
-<<<<<<<< HEAD:app/src/main/java/subspayment.java
 package com.example.myapplication;
 
-========
->>>>>>>> origin/main:app/src/main/java/subsnav.java
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.View;
-import android.view.ViewGroup;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import androidx.annotation.Nullable;
-import androidx.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.example.myapplication.R;
-<<<<<<<< HEAD:app/src/main/java/subspayment.java
-========
-
->>>>>>>> origin/main:app/src/main/java/subsnav.java
-
-public class subspayment extends Fragment{
-    @NonNull
+public class subspayment extends Fragment {
     @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.subspayment_fragment, container, false);
         return view;
